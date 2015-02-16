1ef96544705a576e30e002aaaae7664bcdc8676f
bob <kiksadi@gmail.com>
2015-02-15 23:19:29 -0800

    First grocerylist commit

---
+++ groceries.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+groceries = ['milk','morningthunder']
+print(groceries)
+gitem = raw_input("Enter a grocery item: ")
+print("Adding " + gitem + " to the list")
+groceries.append(gitem)
+print(groceries)
