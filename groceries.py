918cad947fb01d3fd87416542a67cec7d4352903
bob <kiksadi@gmail.com>
2015-02-19 00:15:23 -0800

    Added ability to check for and create file if not found and write to the file

--- groceries.py
+++ groceries.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+# open file for appending and reading, create if doesn't exist
+f = open('glist', 'a+')
+# print file contents below
+print f.read()
+# create empty grocery list
 groceries = []
-print(groceries)
 gitem = raw_input("Enter a grocery item: ")
 print("Adding " + gitem + " to the list")
 groceries.append(gitem)
+f.write(gitem + '\n')
 print(groceries)
+print f.read()
