77b7cb167b123b4a120e03a4bca0d520d43affe2
Bob Gamble <bgamble@costco.com>
2015-02-19 15:18:14 -0800

    Checking for duplicates and uploading test file

--- groceries.py
+++ groceries.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # open file for appending and reading, create if doesn't exist
 f = open('glist', 'a+')
-# print file contents below
-print f.read()
 # create empty grocery list
 groceries = []
 gitem = raw_input("Enter a grocery item: ")
-print("Adding " + gitem + " to the list")
-groceries.append(gitem)
-f.write(gitem + '\n')
+if gitem in f.read():
+    print "You already added " + gitem + "to the list."
+else:
+    print("Adding " + gitem + " to the list")
+    groceries.append(gitem)
+    f.write(gitem + '\n')
 print(groceries)
 print f.read()
