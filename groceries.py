894db144b1cb9b3bd70e20f58198b31fab34dc2f
bob <kiksadi@gmail.com>
2015-02-20 14:22:28 -0800

    Lowercase all user input

--- groceries.py
+++ groceries.py
@@ -3,15 +3,15 @@
 f = open('glist', 'a+')
 # create empty grocery list
 groceries = []
-choice=raw_input("Add grocery items?")
+choice=raw_input("Add grocery items?").lower()
 while choice[0] == 'y':
-    gitem = raw_input("Enter a grocery item: ")
+    gitem = raw_input("Enter a grocery item: ").lower()
     if gitem in f.read():
-        print "You already added " + gitem + "to the list."
+        print "You already added " + gitem + " to the list."
     else:
         print("Adding " + gitem + " to the list")
         groceries.append(gitem)
         f.write(gitem + '\n')
 	print f.read()
-	choice=raw_input("Add another item? ")
+	choice=raw_input("Add another item? ").lower()
 print("Goodbye!")
